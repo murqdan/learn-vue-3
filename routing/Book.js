70689c8cd5fe87cdda45b7e8c0021da4f26dbcbb
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-05-25 13:50:07 +0700

    (vue router) dynamic routing - create a Book component

---
+++ routing/Book.js
@@ -0,0 +1,31 @@
+const Book = {
+    setup (props, context) {
+        const book = Vue.reactive({})
+        const route = VueRouter.useRoute()
+        const { id } = route.params // get params id
+
+        const fetchBook = (id) => {
+            fetch('http://127.0.0.1:5500/books.json')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                const currentBook = data.find(book => book.id == id);
+                if (currentBook) Object.assign(book, { ...currentBook })
+            })
+        }
+
+        if (id > 0) fetchBook(id)
+
+        return {
+            book
+        }
+    },
+    template: 
+    `<div>
+        <h2> Detail Book </h2>
+        <p>{{ book }}</p>
+    </div>`
+}
+
+export default Book
